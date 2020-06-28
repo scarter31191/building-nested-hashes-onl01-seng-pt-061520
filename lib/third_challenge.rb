c1b26e71fc28811b469f618e24e83a4eac4fde6e
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 15:21:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,12 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: 
-        #your key/value pairs here
+      :patriarch => {
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: #your key/value pairs here
         },
       :hero => {
         #your key/value pairs here
